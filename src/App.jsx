3ad933591dacb5cd22fa3261ce8e0ebff5cf73ec
Hewitt02 <Hewitt1203@gmail.com>
2023-11-07 18:46:43 -0500

    Will

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css';
-import BucketList from './components/BucketList';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/about" component={AboutMe} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/resume" component={Resume} />
+        <Route component={AboutMe} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="avatar.jpg" alt="Developer's Avatar" />
+      <p>Hi! My name is William Hewitt. I am currently enrolled in a full stack web developer bootcamp at the University of Pennsylvania. I am passionate about fitness, love to play video games, and as a fun hobby I enjoy photogrpahy and videography.</p>
+    </section>
+  );
+};
+
+export default AboutMe;
